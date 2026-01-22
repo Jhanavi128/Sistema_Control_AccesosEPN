d912f8def839ae275d2d57614f3b6a61200d9e3b
023daniii <agb.dan.zzz@gmail.com>
2026-01-21 21:08:50 -0600

    cambios

--- AccesoApp/src/DataAccess/DAOS/PeriodoDAO.java
+++ AccesoApp/src/DataAccess/DAOS/PeriodoDAO.java
@@ -1,4 +1,10 @@
-public class PeriodoDAO extends DataHelperSQLiteDAO <PeriodoDTO> {
+package DataAccess.DAOs;
+
+import DataAccess.DTOs.PeriodoDTO;
+import DataAccess.Helpers.DataHelperSQLiteDAO;
+import Infrastructure.AppException;
+
+public class PeriodoDAO extends DataHelperSQLiteDAO<PeriodoDTO> {
     public PeriodoDAO() throws AppException {
         super(PeriodoDTO.class, "Periodo", "IdPeriodo");
     }
